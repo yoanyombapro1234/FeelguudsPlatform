48a0e87d9d9c5d3aa319f0732a2319a65fb16311
LensPlatform <yoanyombapro@gmail.com>
2021-09-09 01:19:37 -0400

    fix: update authentication service startup script
    Signed-off-by: LensPlatform <yoanyombapro@gmail.com>

--- scripts/run_authn.sh
+++ scripts/run_authn.sh
@@ -2,7 +2,7 @@
 
 docker run --rm \
   --publish 8000:3000 \
-  -e AUTHN_URL=localhost:8000 \
+  -e AUTHN_URL=http://localhost:8000 \
   -e APP_DOMAINS=localhost \
   -e DATABASE_URL=sqlite3://:memory:?mode=memory\&cache=shared \
   -e SECRET_KEY_BASE='secret' \
